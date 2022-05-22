894acf14bd36bb430dd992f68250a1eb117a8b0f
firasabualkomboz <firasa018@gmail.com>
2022-05-22 18:07:12 +0300

    edit register page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('type')->default(0); /* tenant= 0 , owner = 2 , manager = 1   */
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Manager\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'user-access:manager'])->prefix('manager')->group(function () {
+
+    Route::get('/', [\App\Http\Controllers\Manager\HomeController::class, 'index']);
+
+});
+
+Route::middleware(['auth', 'user-access:owner'])->group(function () {
+
+});
+
+Route::middleware(['auth', 'user-access:tenant'])->group(function () {
+
 });
