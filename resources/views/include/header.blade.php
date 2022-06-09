08f2144f30bea7b4837b36fb552aa021b351ac9e
firasabualkomboz <firasa018@gmail.com>
2022-06-09 14:15:49 +0300

    refactor header in dashboard

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -51,9 +51,11 @@
                      id="kt_quick_user_toggle">
                                 <span
                                     class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Sean</span>
+                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">{{auth()->user()->name}}</span>
                     <span class="symbol symbol-35 symbol-light-success">
-											<span class="symbol-label font-size-h5 font-weight-bold">S</span>
+											<span class="symbol-label font-size-h5 font-weight-bold">
+                                              {{substr(auth()->user()->name , 0 , 1)}}
+                                            </span>
 										</span>
                 </div>
             </div>
